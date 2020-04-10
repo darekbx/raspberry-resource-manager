7f08b8f8971c2fb3a8c79cba9a47c78ec3c57b18
Darek <34578369+darekbx@users.noreply.github.com>
2020-04-10 22:27:42 +0200

    Removed unused resource

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 from app.configuration import Configuration
 
 from flask_sqlalchemy import SQLAlchemy
-from flask_pymongo import PyMongo
 
 configuration = Configuration().load()
 
